fc3e65dc621c3a312575d96ad1b82c1f0e6926d9
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-09-11 19:34:23 -0400

    adventure done :D

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Stack 
 
 import random
 from ast import literal_eval
@@ -29,12 +30,62 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# use this Stack as a way to backtrack
+master_path = []
 
+#store available moves here
+moves = {}
+
+# reverse directions for when going backwards
+reverse_direction = { 
+    'n': 's',
+    's': 'n',
+    'e': 'w',
+    'w': 'e'
+}
+
+# # add current room to visited
+# visited_rooms.add(player.current_room.id)
+# # get exits to start traversal
+moves[player.current_room.id] = player.current_room.get_exits()
+
+# traverse if length of visited < length of rooms in world
+while len(moves) < len(world.rooms) - 1:
+
+    # check if current room id is in visited
+    if player.current_room.id not in moves:
+        room = player.current_room.id
+        # add the room to visited
+        
+        #grab previous direction
+        moves[room] = player.current_room.get_exits()
+        back = master_path[-1]
+        #remove previous direction to avoid that direction
+        moves[player.current_room.id].remove(back)
+    # change traversal
+    while len(moves[player.current_room.id]) == 0:
+        #remove previous exits set
+        back = master_path.pop()
+        # add last set of exits
+        traversal_path.append(back)
+        # use travel function to move to previous room
+        player.travel(back)
+
+    # check current_room's exits and find last room on list, go to that room.
+    # append to path as this is the right directions
+    next_move = moves[player.current_room.id].pop(0)
+    # append it to record of going there
+    traversal_path.append(next_move)
+    master_path.append(reverse_direction[next_move])
+    # use the directions dictionary to go backwards through rooms
+    player.travel(next_move)
 
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
+
+
 
 for move in traversal_path:
     player.travel(move)
